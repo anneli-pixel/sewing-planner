788048b2b2a7a881a9701a8a058ad5fe4e347753
Anne Lißner <anne_lissner@gmx.net>
2020-11-26 16:54:12 +0100

    Add basic project show page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,4 +5,8 @@ class ProjectsController < ApplicationController
     @projects = policy_scope(Project)
   end
 
+  def show
+    @project = Project.find(params[:id])
+    authorize @project
+  end
 end
