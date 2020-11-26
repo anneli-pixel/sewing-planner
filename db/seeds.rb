7a8112cf7e8325c89faec529b0cba8de806f8f2b
Anne Lißner <anne_lissner@gmx.net>
2020-11-26 16:55:28 +0100

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@
 
 Project.destroy_all
 User.destroy_all
+Pattern.destroy_all
 
 require 'open-uri'
 
@@ -18,28 +19,51 @@ anne = User.new(
   )
 anne.save
 
+# Create Patterns
+pattern1 = Pattern.new(
+  title: "Korfu Crop Top",
+  designer: "Self Drafted",
+  fabric_type: "Woven",
+  garment_category: "Tops"
+  )
+pattern1.save
+
+pattern2 = Pattern.new(
+  title: "Dark Grey Jeans",
+  designer: "123 Patterns",
+  fabric_type: "Knit",
+  garment_category: "Pants"
+  )
+pattern2.save
+
 # Create Projects
 project1 = Project.new(
   title: 'Blue Summer Top',
-  status: 'draft',
+  status: 'Draft',
+  garment_category: 'Tops',
   description: 'Use Korfu crop top pattern and make it a bit shorter?',
-  user_id: anne.id
+  user_id: anne.id,
+  pattern_id: pattern1.id
   )
 project1.save
 
 project2 = Project.new(
   title: 'Red High-Waist Jeans',
-  status: 'in progress',
+  status: 'In progress',
+  garment_category: 'Pants',
   description: 'Use dark grey pants pattern as basis.',
-  user_id: anne.id
+  user_id: anne.id,
+  pattern_id: pattern2.id
   )
 project2.save
 
 project3 = Project.new(
   title: 'Pineapple Summer Top',
-  status: 'completed',
+  status: 'Completed',
+  garment_category: 'Tops',
   description: 'Pink viscose from Barcelona plus green bias binding.',
   adjustments: 'Increased bust circumference and made it a bit longer.',
-  user_id: anne.id
+  user_id: anne.id,
+  pattern_id: pattern1.id
   )
 project3.save
