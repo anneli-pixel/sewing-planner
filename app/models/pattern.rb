08f6c8c5bc55aca4b0325de5c513ea948c574c7d
Anne Lißner <anne_lissner@gmx.net>
2020-12-02 18:22:46 +0100

    refactor validations and add user association

--- app/models/pattern.rb
+++ app/models/pattern.rb
@@ -1,7 +1,10 @@
 class Pattern < ApplicationRecord
+  FABRIC_TYPES = %w(Knit Woven)
+  GARMENT_CATEGORIES = %w(Pants Pullover/Cardigans Tops Dresses Skirts Underwear Outerwear)
   has_many :projects
-  validates :title, presence: true
+  belongs_to :user, optional: true
+  validates :title, presence: true, uniqueness: { scope: :user_id }
   validates :designer, presence: true
-  validates :fabric_type, presence: true, inclusion: { in: %w(Knit Woven), message: "must be Knit or Woven." }
-  validates :garment_category, inclusion: { in: ["Pants", "Tops", "Dresses", "Skirts", "Underwear", "Outerwear"] }
+  validates :fabric_type, presence: true, inclusion: { in: FABRIC_TYPES, message: "must be #{FABRIC_TYPES[0..-2].join(", ")} or #{FABRIC_TYPES.last}."  }
+  validates :garment_category, inclusion: { in: GARMENT_CATEGORIES, message: "must be #{GARMENT_CATEGORIES[0..-2].join(", ")} or #{GARMENT_CATEGORIES.last}." }
 end
