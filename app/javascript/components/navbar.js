3063588c1872a8722582081785e42edf71144fda
Anne Lissner <anne.lissner@redbubble.com>
2020-12-14 18:16:06 +0100

    add navbar with icons

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,9 +1,9 @@
 const makeNavbarItemActiveOnClick = () => {
-  const navItems = document.querySelectorAll(".navbar-nav .nav-link");
+  const navItems = document.querySelectorAll(".new-navbar .new-nav-item");
   const url = window.location.href;
 
   navItems.forEach((category) => {
-    if (url.includes(category)) {
+    if (url.includes(category.id)) {
       category.classList.add("active");
     };
   });
