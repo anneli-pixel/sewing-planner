90e970b55eaad188aae52095d5c8eb12c48369cb
Anne Lissner <anne.lissner@redbubble.com>
2020-12-15 18:54:10 +0100

    remove unused associations

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,13 +3,11 @@ class Project < ApplicationRecord
   validates :title, presence: true, uniqueness: { scope: :user_id }
   validates :size, presence: true
   validates :status, inclusion: { in: STATUSES, message: "must be #{Project::STATUSES[0..-2].join(", ")} or #{Project::STATUSES.last}." }
-  #validates :garment_category, inclusion: { in: ["Pants", "Tops", "Dresses", "Skirts", "Underwear", "Outerwear"] }
   belongs_to :user
   belongs_to :pattern
   has_many :shopping_items
   has_many :fabrics
-  has_many :project_fabrics
-  has_many :fabrics, through: :project_fabrics
+
 
   def start_date
     # self.start_date = self.created_at
