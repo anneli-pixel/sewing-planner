9fd252f05e3bc231b68e39b82630f1da873c1147
Anne Lissner <anne.lissner@redbubble.com>
2020-12-15 17:45:15 +0100

    remove redundant fabric type column from schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_15_133842) do
+ActiveRecord::Schema.define(version: 2020_12_15_164333) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "fabrics", force: :cascade do |t|
     t.string "title"
-    t.string "fabric_type"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
