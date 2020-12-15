c3908fb20d7d15fa028091a958baf0834388bbe6
Anne Lissner <anne.lissner@redbubble.com>
2020-12-15 18:17:38 +0100

    add association tests

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Project, type: :model do
 
+  describe "Associations" do
+    it { should belong_to(:user) }
+    it { should belong_to(:pattern) }
+    it { should have_many(:fabrics) }
+  end
 end
