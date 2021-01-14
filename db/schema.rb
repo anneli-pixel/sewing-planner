cb720414542e8715307b0f8bf16677beea8a322b
Anne Lißner <anne_lissner@gmx.net>
2021-01-14 18:27:03 +0100

    add user reference migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_15_180141) do
+ActiveRecord::Schema.define(version: 2021_01_14_172404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,7 +77,9 @@ ActiveRecord::Schema.define(version: 2020_12_15_180141) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
+    t.bigint "user_id", null: false
     t.index ["project_id"], name: "index_shopping_items_on_project_id"
+    t.index ["user_id"], name: "index_shopping_items_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -101,4 +103,5 @@ ActiveRecord::Schema.define(version: 2020_12_15_180141) do
   add_foreign_key "projects", "patterns"
   add_foreign_key "projects", "users"
   add_foreign_key "shopping_items", "projects"
+  add_foreign_key "shopping_items", "users"
 end
