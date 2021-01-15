4035fc31a9962e4a3ad58f6b0c5040b35b3c4823
Anne Lißner <anne_lissner@gmx.net>
2021-01-15 12:35:44 +0100

    add basic form in view

--- app/controllers/shopping_items_controller.rb
+++ app/controllers/shopping_items_controller.rb
@@ -1,7 +1,12 @@
 class ShoppingItemsController < ApplicationController
   def new
+    @shopping_item = ShoppingItem.new
+    authorize @shopping_item
   end
 
+  def create
+
+  end
   def index
   end
 end
