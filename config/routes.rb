642c6e82e8fcdaa4d5ac96dc63e953fd3b804529
Anne Lissner <anne.lissner@redbubble.com>
2021-01-15 14:52:09 +0100

    remove redundant routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'shopping_items/new'
-  get 'shopping_items/index'
+
   devise_for :users
   root to: 'projects#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
