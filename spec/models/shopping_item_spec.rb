61299e60c3e9d62f1c7238f7d6f02ae2759460c5
Anne Lißner <anne_lissner@gmx.net>
2021-01-15 10:54:36 +0100

    valid with valid attributes

--- spec/models/shopping_item_spec.rb
+++ spec/models/shopping_item_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe ShoppingItem, type: :model do
-
+    subject do
+      user = User.create(email: "test@example.com", password: "Fd392&sKx?")
+      project = Project.new(title: "Crop Top",
+                            status: "Draft",
+                            size: "M",
+                            user_id: user.id
+                            )
+      shopping_item = ShoppingItem.new(name: "10 Buttons",
+                                       user_id: user.id,
+                                       project_id: project.id)
+    end
 
   describe "Associations" do
     it { should belong_to(:user) }
@@ -10,5 +20,17 @@ RSpec.describe ShoppingItem, type: :model do
 
   describe "Validations" do
     it { should validate_presence_of(:name) }
+  end
+
+  describe "Valid attributes" do
+
+    it "is valid without a project" do
+      subject.project_id = nil
+      expect(subject).to be_valid
+    end
+
+    it "is valid with a project" do
+      expect(subject).to be_valid
+    end
   end
 end
