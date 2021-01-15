8455fc1d4385bd34d7a296dd0d7325b02af4163b
Anne Lißner <anne_lissner@gmx.net>
2021-01-15 12:58:26 +0100

    add index and buggy create method

--- app/controllers/shopping_items_controller.rb
+++ app/controllers/shopping_items_controller.rb
@@ -5,8 +5,22 @@ class ShoppingItemsController < ApplicationController
   end
 
   def create
-
+    @shopping_item = ShoppingItem.new(shopping_item_params)
+    authorize @shopping_item
+    @shopping_item.user = current_user
+    shopping_item_params[:project_id].empty? ? @shopping_item.project_id = nil : @shopping_item.project_id = shopping_item_params[:project_id]
+    if @shopping_item.save
+      redirect_to shopping_items_index_path, notice: "Shopping item successfully created."
+    else
+      render :new
+    end
   end
+
   def index
+    @shopping_items = policy_scope(ShoppingItem)
+  end
+
+  def shopping_item_params
+    params.require(:shopping_item).permit(:name, :project_id)
   end
 end
