d704b4b0c38a49f5e3f8f23c6a0e9f12452451ce
Anne Lißner <anne_lissner@gmx.net>
2021-01-15 12:57:48 +0100

    add bare bone spec without any functioning test for now

--- spec/features/new_shopping_item_spec.rb
+++ spec/features/new_shopping_item_spec.rb
@@ -1,2 +1,26 @@
 require 'rails_helper'
 
+RSpec.feature "New Shopping Item Page" do
+  before(:example) do
+    user = User.create!(:email => 'test@example.com', :password => 'f4k3p455w0rd')
+    login_as(user, :scope => :user)
+    visit shopping_items_path
+  end
+
+  after(:example) do
+    Warden.test_reset!
+  end
+
+  describe "Form creates new shopping item:" do
+    it "for a project" do
+      before_count = ShoppingItem.count
+      within("#new_shopping_item") do
+
+      end
+    end
+
+    it "without a project" do
+
+    end
+  end
+end
