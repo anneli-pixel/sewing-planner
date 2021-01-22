5d5d72fa364fef2f9a80c7a9e75920278926f0a0
Anne Lissner <anne.lissner@redbubble.com>
2021-01-22 17:24:01 +0100

    make buy shopping item test pass

--- spec/features/buy_shopping_item_spec.rb
+++ spec/features/buy_shopping_item_spec.rb
@@ -2,38 +2,22 @@ require'rails_helper'
 
 RSpec.feature "Buy Shopping Item" do
 
-  let!(:user) do
+  let(:user) do
     User.create!(email: 'test@example.com',
-                             password: 'f4k3p455w0rd')
+                 password: 'f4k3p455w0rd')
   end
 
   let!(:shopping_item) do
     ShoppingItem.create!(name: "3 Buttons",
-                                             bought: false,
-                                             user: user)
+                         bought: false,
+                         user: user)
   end
 
   it "Can be checked as bought" do
-    # user = User.create!(email: 'test@example.com',
-    #                      password: 'f4k3p455w0rd')
-    # @shopping_item = ShoppingItem.create!(name: "3 Buttons",
-    #                                      bought: false,
-    #                                      user: user)
     login_as(user, :scope => :user)
-
     visit shopping_items_path
-    before_bought = shopping_item.bought
-    puts "BEFORE"
-    puts shopping_item.bought
     find_link(id: "#{shopping_item.id}").click
-    #click_link(id: "#{@shopping_item.id}")
-    print page.html
-    puts "AFTER"
-    puts shopping_item
-    puts shopping_item.bought
-    expect(shopping_item.bought).to be(true)
-    #expect(link_image).to have_css('item-bought true')
-
+    expect(shopping_item.reload.bought).to be(true)
   end
 
   after(:example) do
