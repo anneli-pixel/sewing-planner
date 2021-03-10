9189707a7c2bf7a22102255e26ace60bdfe9fd76
Anne Lissner <anne.lissner@redbubble.com>
2021-03-10 19:18:52 +0100

    remove PatternHelper and use GarmentCategory model instead

--- app/models/garment_category.rb
+++ app/models/garment_category.rb
@@ -2,4 +2,18 @@ class GarmentCategory < ApplicationRecord
   has_many :patterns
 
   validates :name, presence: true
+
+  def image
+    ActionController::Base.helpers.image_tag self.image_path, class: "garment-category-icon"
+  end
+
+  def image_path
+    "icons/#{self.name.downcase.gsub("/","_")}.png"
+  end
+
+  def normalised_name
+    self.name.downcase.gsub("/","_")
+  end
+
+
 end
