d9bced1937562b727347c432f3ab055a42a1233b
Anne Lissner <anne.lissner@redbubble.com>
2021-03-11 19:26:16 +0100

    include js behaviour for all buttons and refactor

--- app/javascript/components/search_bar.js
+++ app/javascript/components/search_bar.js
@@ -1,12 +1,30 @@
 const selectActiveFilter = () => {
-  const queryString = window.location.search;
-  if (queryString) {
-    const urlParams = new URLSearchParams(queryString);
+  const filter = window.location.search;
+  if (filter) {
+    const urlParams = new URLSearchParams(filter);
+    selectGarmentCategoryIn(urlParams);
+    selectFabricTypeIn(urlParams);
+  }
+}
 
-    const garmentCategoryFilter = urlParams.get('garment_category_filter');
+const selectGarmentCategoryIn = (urlParams) => {
+  const garmentCategoryFilter = urlParams.get('garment_category_filter');
+  if (garmentCategoryFilter) {
+  const selectedGarmentCategoryImage = document.getElementById(garmentCategoryFilter);
+  const allLabel = document.getElementById("label-garment-category-all");
+  selectedGarmentCategoryImage.classList.add("selected");
+  allLabel.classList.remove("selected");
+  }
+}
 
-    const selectedGarmentCategoryImage = document.getElementById(garmentCategoryFilter);
-    selectedGarmentCategoryImage.classList.add("selected");
+ const selectFabricTypeIn = (urlParams) => {
+  const fabricTypeFilter = urlParams.get('fabric_type_filter');
+  if (fabricTypeFilter) {
+    const selectedFabricTypeLabel = document.getElementById(`label-${fabricTypeFilter}`);
+    const allLabel = document.getElementById("label-fabric-type-all");
+    console.log(selectedFabricTypeLabel);
+    selectedFabricTypeLabel.classList.add("selected");
+    allLabel.classList.remove("selected");
   }
 }
 
