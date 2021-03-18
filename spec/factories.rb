534bd7f7bc15404f3988a71b70b1d55ec8461a86
Anne Lissner <anne.lissner@redbubble.com>
2021-03-18 15:20:57 +0100

    add factories for models

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,10 +1,33 @@
 FactoryBot.define do
   factory :user do
-    email { "test@example.com" }
+    sequence :email do |n|
+      "test#{n}@example.com"
+    end
     password { "Fd392&sKx?" }
   end
 
   factory :garment_category do
     name { "Pullover/Cardigans" }
+  end
+
+  factory :pattern do
+    association :user
+    association :garment_category
+    title { "Driftless Cardigan" }
+    designer { "Grainline Studio" }
+    fabric_type { "Knit" }
+  end
+
+  factory :project do
+    association :user
+    association :pattern
+    title { "Blue Winter Cardigan" }
+    size { "M" }
+    status { "Draft" }
+  end
+
+  factory :fabric do
+    association :project
+    title { "Blue Wool Cotton Interlock Knit" }
   end
 end
