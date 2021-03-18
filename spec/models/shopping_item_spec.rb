4e55ddc31b8a4b15b8889e63ff7ad69e0e49b669
Anne Lissner <anne.lissner@redbubble.com>
2021-03-18 15:43:55 +0100

    replace subject with factory model

--- spec/models/shopping_item_spec.rb
+++ spec/models/shopping_item_spec.rb
@@ -1,18 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe ShoppingItem, type: :model do
-    subject do
-      user = User.create(email: "test@example.com", password: "Fd392&sKx?")
-      project = Project.new(title: "Crop Top",
-                            status: "Draft",
-                            size: "M",
-                            user_id: user.id
-                            )
-      shopping_item = ShoppingItem.new(name: "10 Buttons",
-                                       bought: false,
-                                       user: user,
-                                       project: project)
-    end
+  subject { create(:shopping_item) }
 
   describe "Associations" do
     it { should belong_to(:user) }
@@ -27,5 +16,4 @@ RSpec.describe ShoppingItem, type: :model do
     subject.buy
     expect(subject.bought).to eq(true)
   end
-
 end
