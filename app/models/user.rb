d89e9f2874d512f7f5e8a49fe84e781df4360d20
Anne Lissner <anne.lissner@redbubble.com>
2021-03-25 12:47:26 +0100

    fix typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :measurements, :dependent :destroy
+  has_many :measurements, dependent: :destroy
   has_many :fabrics, dependent: :destroy
   has_many :projects, dependent: :destroy
   has_many :patterns, dependent: :destroy
