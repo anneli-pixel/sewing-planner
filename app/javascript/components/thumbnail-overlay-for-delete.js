29420d7208b04d831d4413e317c5891b1ff3ea3c
Anne Lissner <anne.lissner@redbubble.com>
2021-04-29 15:52:27 +0200

    limit js to new and edit pages

--- app/javascript/components/thumbnail-overlay-for-delete.js
+++ app/javascript/components/thumbnail-overlay-for-delete.js
@@ -1,10 +1,16 @@
 const thumbnailOverlayForDelete = () => {
-  const thumbnailOverlay = document.querySelector('.thumbnail-overlay');
-  const deleteButton = document.querySelector('.delete-button');
 
-  deleteButton.addEventListener(('click'), () => {
-      thumbnailOverlay.classList.toggle('selected');
-  });
+  const pageId = document.querySelector('body').children[1].id;
+  const pageIdsWithThumbnails = ['projects-edit', 'projects-new', 'patterns-edit', 'patterns-new'];
+
+  if (pageIdsWithThumbnails.includes(pageId)) {
+    const thumbnailOverlay = document.querySelector('.thumbnail-overlay');
+    const deleteButton = document.querySelector('.delete-button');
+  
+    deleteButton.addEventListener(('click'), () => {
+        thumbnailOverlay.classList.toggle('selected');
+    });
+  }
 }
 
 export { thumbnailOverlayForDelete };
