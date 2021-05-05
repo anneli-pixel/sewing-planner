5e71b49d48fd3b96f6d9710412bedd97ee653e8f
Anne Lissner <anne.lissner@redbubble.com>
2021-05-05 13:26:03 +0200

    limit js to pattern new and edit only

--- app/javascript/components/flip_card_garment_category_icon.js
+++ app/javascript/components/flip_card_garment_category_icon.js
@@ -1,13 +1,18 @@
 const addFlipCardClassesForGarmentCategoryIcon = () => {
-  const flipCards = document.querySelectorAll(".category-item");
-  flipCards.forEach((flipCard) => {
-    flipCard.classList.add('flip-card');
-  })
+  const pageId = document.querySelector('body').children[1].id;
+  const pageIdsWithFlipCards = ['patterns-edit', 'patterns-new'];
 
-  const innerFlipCards = document.querySelectorAll('.flip-card > label');
-  innerFlipCards.forEach((innerFlipCard) => {
-    innerFlipCard.classList.add('flip-card-inner');
-  })
+  if (pageIdsWithFlipCards.includes(pageId)) {
+    const flipCards = document.querySelectorAll(".category-item");
+    flipCards.forEach((flipCard) => {
+      flipCard.classList.add('flip-card');
+    });
+
+    const innerFlipCards = document.querySelectorAll('.flip-card > label');
+    innerFlipCards.forEach((innerFlipCard) => {
+      innerFlipCard.classList.add('flip-card-inner');
+    });
+  }
 }
 
 export { addFlipCardClassesForGarmentCategoryIcon };
