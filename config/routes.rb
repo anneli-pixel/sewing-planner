68ae535888708470ff57657d81c48f4de3c6a79d
Anne Lissner <anne.lissner@redbubble.com>
2021-05-19 17:55:04 +0200

    add basic homepage for logged in user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
+  get 'home', to: 'pages#home'
   devise_for :users
-  root to: 'projects#index'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :projects do
     patch "change_status", on: :member
