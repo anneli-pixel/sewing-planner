67ab3f030ae88bb0581da6d7f503b6cd2ab4b5fc
Anne Lißner <anne_lissner@gmx.net>
2021-08-26 17:30:58 +0200

    hide icons when not logged in and add navbar title

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,12 +1,12 @@
 const makeNavbarItemActiveOnClick = () => {
-  const navItems = document.querySelectorAll(".custom-navbar .nav-item");
+  const navItems = document.querySelectorAll(".custom-navbar-icons .nav-item");
   const url = window.location.href;
 
   navItems.forEach((category) => {
     if (url.includes(category.id)) {
       category.classList.add("active");
-    };
+    }
   });
-}
+};
 
 export { makeNavbarItemActiveOnClick };
